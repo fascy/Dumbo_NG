a73e3310b84bf8004c79a7ea6f2b21cce0bcabf4
fascy <fascy97@163.com>
2021-12-21 15:49:31 +0800

    buf size

--- network/socket_server.py
+++ network/socket_server.py
@@ -41,9 +41,9 @@ class NetworkServer (Process):
             try:
                 while not self.stop.value:
                     if self.win == 1:
-                        buf += sock.recv(53248)
+                        buf += sock.recv(106496)
                     else:
-                        buf += sock.recv(26624)
+                        buf += sock.recv(53248)
                     tmp = buf.split(self.SEP, 1)
                     while len(tmp) == 2:
                         buf = tmp[1]
